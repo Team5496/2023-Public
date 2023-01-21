562958262995f668c84d7bb1ca103163581af5a3
Birb12 <minedragocraft@gmail.com>
2023-01-21 15:02:24 -0800

    vision integration, now drives in the correct direction (lol)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,20 +31,20 @@ public final class Constants {
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 2; //  Set front left module drive motor ID
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 4; //  Set front left module steer motor ID
     public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 10; //  Set front left steer encoder ID
-    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(332.46002197265625); //  Measure and set front left steer offset
+    public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(342.06210937500003 + 20 + 180); //  Measure and set front left steer offset
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 5; //  Set front right drive motor ID
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 3; //  Set front right steer motor ID
     public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 9; //  Set front right steer encoder ID
-    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(202.752685546875); //  Measure and set front right steer offset
+    public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(204.52031250000002 + 20 + 180); //  Measure and set front right steer offset
 
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 6; //  Set back left drive motor ID
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 1; //  Set back left steer motor ID
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 12; //  Set back left steer encoder ID
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(331.34765625); //  Measure and set back left steer offset
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(331.52109375000003 + 20 + 180); //  Measure and set back left steer offset
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 8; //  Set back right drive motor ID
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 7; //  Set back right steer motor ID
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11; //  Set back right steer encoder ID
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(87.01171875); //  Measure and set back right steer offset
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(86.042578125 + 20 + 180); //  Measure and set back right steer offset
 } 
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import frc.robot.subsystems.Limelight;
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj.Joystick;
@@ -23,7 +24,8 @@ public class Robot extends TimedRobot {
   private RobotContainer m_robotContainer;
   private Command[] m_autonomousCommands = new Command[2];
   private final Joystick m_codriver = new Joystick(1);
-  private DoubleSolenoid scissor = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+  private Boolean activated = false;
+  private Limelight limelight = new Limelight("gloworm");
 
 
   /**
@@ -51,6 +53,11 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    limelight.readPeriodically();
+
+    if (limelight.getCameraToTarget() != null) {
+      limelight.printID();
+    }
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
@@ -79,10 +86,9 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
+    Command zeromotors = m_robotContainer.m_drivetrainSubsystem.zeromotors();
+    zeromotors.schedule();
+
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
@@ -92,12 +98,6 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    scissor.set(kReverse);
-    Boolean activated = false;
-    if (m_codriver.getRawButton(1) && activated == false){
-      activated = true;
-      scissor.set(kForward);
-    } else if (m_codriver.getRawButton(1) && activated == true) {scissor.set(kReverse); activated = false;}
   }
 
   @Override

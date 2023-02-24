51b34ada424f6661def101a124c7562ddc9502c6
mariohydra2288 <mariohydra2288@outlook.com>
2023-02-23 19:04:50 -0800

    fixes

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -2,91 +2,117 @@ package frc.robot.subsystems;
 
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
+import org.photonvision.RobotPoseEstimator;
+import org.photonvision.RobotPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.geometry.Rotation3d;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Transform3d;
-import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.util.Units;
-import java.util.Optional;
-import org.photonvision.EstimatedRobotPose;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Limelight
 {
     private PhotonCamera camera;
     private PhotonPipelineResult result;
-    Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0));
-    NetworkTable limelightTable;
-    NetworkTableEntry tx, ty, ta;
-    AprilTagFieldLayout aprilTagFieldLayout;
-    PhotonPoseEstimator poseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, camera, robotToCam);
-
-   
     
+    NetworkTable limelightTable;
+    double tx, ty, tv;
+    AprilTagFieldLayout aprilTagFieldLayout;
 
     public Limelight(String name) {
         camera = new PhotonCamera(name);
         result = camera.getLatestResult();
-        limelightTable = NetworkTableInstance.getDefault().getTable("gloworm");
-        tx = limelightTable.getEntry("tx");
-        ty = limelightTable.getEntry("ty");
-        ta = limelightTable.getEntry("ta");
-        
+        limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
+
+
+        //tx = limelightTable.getEntry("tx");
+        //ty = limelightTable.getEntry("ty");
+        //ta = limelightTable.getEntry("ta");
+
         try {
-            aprilTagFieldLayout = new AprilTagFieldLayout("april.json");
+            aprilTagFieldLayout = new AprilTagFieldLayout(Filesystem.getDeployDirectory() + "AprilTagFieldLayout.json");
         } catch (IOException e) {
             System.out.println("File not found");
             e.printStackTrace();
         }
-        
     }
 
     //for testing input from camera and output on computer
-    public int getID() {
+    public void getTargetInformation() {
         if(result.hasTargets())
         {        
-            return result.getBestTarget().getFiducialId();
-        }
+            SmartDashboard.putNumber("id", result.getBestTarget().getFiducialId());
+            SmartDashboard.putNumber("yaw", result.getBestTarget().getYaw());
+            SmartDashboard.putNumber("pitch", result.getBestTarget().getPitch());
+            SmartDashboard.putNumber("skew", result.getBestTarget().getSkew());
+            SmartDashboard.putNumber("area", result.getBestTarget().getArea());
 
-        return 0;
+            SmartDashboard.putNumber("tv", tv);
+            SmartDashboard.putNumber("tx", tx);
+        }
     }
 
     //gets the transform that maps the camera space to the tag space
     public Transform3d getCameraToTarget() {
-        if (result.hasTargets()) {
-            System.out.println(result.getBestTarget().getBestCameraToTarget().getTranslation().getX());
-            return result.getBestTarget().getBestCameraToTarget();
-        } 
-
-        return null;
+        if(result.hasTargets()) {
+            Transform3d transform = result.getBestTarget().getBestCameraToTarget();
+            Translation3d translation = transform.getTranslation();
+            translation.getX();
+        }
+        return result.hasTargets() ? result.getBestTarget().getBestCameraToTarget()
+            : null; 
     }
-
-    public Optional<EstimatedRobotPose> getRobotPoseAprilTag(Pose2d prevEstimatedRobotPose) {
-        poseEstimator.setReferencePose(prevEstimatedRobotPose);
-        return poseEstimator.update();
+    public boolean isCentered() {
+        if(result.hasTargets()) {
+            if(result.getBestTarget().getYaw() == 0.0 && result.getBestTarget().getSkew() == 0.0) {
+                return true;
+            }
+        }
+        return false;
     }
+    //where the first object is yaw, and the second is skew
+    public double[] getDistanceToCenter() {
+        double[] returnValue = new double[2];
+        if(result.hasTargets()) {
+            returnValue[0] = result.getBestTarget().getSkew();
+            returnValue[1] = result.getBestTarget().getYaw();
+            System.out.println(returnValue[0] + " " + returnValue[1]);
+        }
+        else {
+            System.out.println("No target");
+        }
+        return returnValue;
+    }   
 
+    //gets the distance from the camera to the target
     public double getDistanceToTarget() {
         if (result.hasTargets()) {
             return PhotonUtils.calculateDistanceToTargetMeters(
-                1.18,
+                1.22,
                 0.36,
                 0.0,
                 Units.degreesToRadians(result.getBestTarget().getPitch()));
         }
         return 0.0;
+    }
+    public int getID() {
+        return result.hasTargets() ? result.getBestTarget().getFiducialId()
+            : 0;
     }
 
 
@@ -99,9 +125,15 @@ public class Limelight
     //values that need to be periodically read/updated
     public void readPeriodically() {
         this.result = camera.getLatestResult();
-        tx.getDouble(0.0);
-        ty.getDouble(0.0);
-        ta.getDouble(0.0);
+        tx = limelightTable.getEntry("tx").getDouble(0.0);
+        ty = limelightTable.getEntry("ty").getDouble(0.0);
+        tv = limelightTable.getEntry("tv").getDouble(0.0);
+
+        for(String str : limelightTable.getKeys()) {
+            System.out.println(str);
+        }
+        //ty.getDouble(0.0);
+        //ta.getDouble(0.0);
         //System.out.println("Pipeline" + camera.getPipelineIndex());
     }
 }

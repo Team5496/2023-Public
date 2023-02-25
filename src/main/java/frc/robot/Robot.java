e581d91e8fd0c55e5fa10e30af2160ec542e1add
Birb12 <minedragocraft@gmail.com>
2023-02-24 19:03:42 -0800

    elevator stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -28,6 +28,8 @@ import frc.robot.subsystems.Elevator;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import frc.robot.subsystems.Arm;
 import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -64,6 +66,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    elevator.resetEncoderPosition();
 
     m_robotContainer = new RobotContainer();    
     m_colorMatcher.addColorMatch(colorPurple);
@@ -140,12 +143,12 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    elevator.resetEncoderPosition();
     /* 
     actuator.setBounds(2.0, 1.8, 1.5, 1.2, 1.0);
     actuator.set(1.0);
     
     */
+
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
@@ -178,20 +181,21 @@ public class Robot extends TimedRobot {
     }
     */
 
+    elevator.elevatorSmartDashboard();
 
+     
     if (m_normaldriver.getAButtonPressed()) {
-      elevator.goUp();
+      elevator.setPosition(1200);
     } else if (m_normaldriver.getBButtonPressed()) {
-      elevator.goDown();
+      elevator.setPosition(500);
     }
-
-    
+    /* 
     if (magnet.get()) {elevator.goUp();}
     else {
       elevator.goDown();
     }
 
-    /* 
+    
     limelight.readPeriodically();
     if (limelight.getCameraToTarget() != null) {
       switch(recordedapriltagID)
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import com.revrobotics.SparkMaxPIDController;
+import com.ctre.phoenix.sensors.CANCoderStatusFrame;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -10,7 +12,7 @@ import frc.robot.Constants;
 
 public class Elevator {
     
-    private CANSparkMax l_motor, f_motor;
+    public CANSparkMax l_motor, f_motor;
     private SparkMaxPIDController l_motorController;
     private RelativeEncoder l_motorEncoder, f_motorEncoder;
     public double kP, kI, kD, kF, kMaxOutput, kMinOutput;
@@ -22,24 +24,22 @@ public class Elevator {
 
         l_motorController = l_motor.getPIDController();
         l_motorEncoder = l_motor.getEncoder();
-       // l_motorEncoder.setPositionConversionFactor(100);
-        //l_motor.setClosedLoopRampRate(0.1);
+        l_motorEncoder.setPositionConversionFactor(100);
+        l_motor.setClosedLoopRampRate(.1);
 
         f_motor.follow(l_motor, true);
         f_motorEncoder = f_motor.getEncoder();
-       // f_motorEncoder.setPositionConversionFactor(100);
+        f_motorEncoder.setPositionConversionFactor(100);
 
-        /* 
         l_motorController.setP(Constants.e_KP, 0);
         l_motorController.setI(Constants.e_KI, 0);
         l_motorController.setD(Constants.e_KD, 0);
         l_motorController.setFF(Constants.e_KF, 0);
         l_motorController.setOutputRange(Constants.e_OUTPUT_MIN, Constants.e_OUTPUT_MAX, 0);
-        */
     }
 
     public void setPosition(double position) {
-        l_motorController.setReference(position, CANSparkMax.ControlType.kPosition, 0);
+        l_motorController.setReference(position, CANSparkMax.ControlType.kPosition);
     }
     public double getPosition() {
         return l_motorEncoder.getPosition();
@@ -49,18 +49,28 @@ public class Elevator {
         l_motorEncoder.setPosition(0);
         f_motorEncoder.setPosition(0);
     }
+
+    public void driveJoystick(double y) {
+        l_motor.set(y);
+    }
     
     public void goUp() {
-        l_motor.set(0.25);
+        l_motorController.setReference(150.0, CANSparkMax.ControlType.kPosition);
     }
 
     public void goDown() {
-        l_motor.set(-0.25);
+        l_motorController.setReference(-150.0, CANSparkMax.ControlType.kPosition);
     }
+
+    public void hold() {
+        l_motorController.setReference(l_motorEncoder.getPosition(), CANSparkMax.ControlType.kVelocity);
+    }
+
     public void elevatorSmartDashboard() {
         SmartDashboard.putNumber("Lead Position", l_motorEncoder.getPosition());
         SmartDashboard.putNumber("Graph Position", l_motorEncoder.getPosition());
         SmartDashboard.putNumber("Follower Position", f_motorEncoder.getPosition());
         SmartDashboard.putNumber("Follower Velocity", f_motorEncoder.getVelocity());
+        SmartDashboard.putNumber("Output Current", l_motor.getOutputCurrent());
     }
 }

999dddcc7dd00820f50a7684bbae3c2ce852676b
stitchhydra2288 <stitchhydra2288@outlook.com>
2023-03-28 17:55:34 -0700

    new sds lib

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -13,7 +13,8 @@ import com.pathplanner.lib.commands.*;
 import frc.robot.Constants;
 import java.util.function.Supplier;
 import edu.wpi.first.math.controller.PIDController;
-import com.swervedrivespecialties.swervelib.Mk4iSwerveModuleHelper;
+import com.swervedrivespecialties.swervelib.MkSwerveModuleBuilder;
+import com.swervedrivespecialties.swervelib.MotorType;
 import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
 import edu.wpi.first.wpilibj2.command.Command;
 import com.swervedrivespecialties.swervelib.SwerveModule;
@@ -84,18 +85,15 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
   // positions are instantiated as 0
   public SwerveModule m_frontLeftModule;
-  public SwerveModulePosition m_frontleftPosition = new SwerveModulePosition(0, new Rotation2d(0, 0));
   public SwerveModule m_frontRightModule;
-  public SwerveModulePosition m_frontRightPosition = new SwerveModulePosition(0, new Rotation2d(0, 0));
   public SwerveModule m_backLeftModule;
-  public SwerveModulePosition m_backleftPosition = new SwerveModulePosition(0, new Rotation2d(0, 0));
   public SwerveModule m_backRightModule;
-  public SwerveModulePosition m_backRightPosition = new SwerveModulePosition(0, new Rotation2d(0, 0));
-  public SwerveModulePosition[] positions = new SwerveModulePosition[]{m_frontleftPosition, m_frontRightPosition, m_backleftPosition, m_backRightPosition};
-  
-  public SwerveDrivePoseEstimator m_odometry = new SwerveDrivePoseEstimator(
-        m_kinematics, getGyroscopeRotation(), positions, m_pose);
-        
+  public SwerveModulePosition[] positions;
+  private final SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(
+        m_kinematics,
+        getGyroscopeRotation(),
+        positions
+);     
      
  
   private ChassisSpeeds m_chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
@@ -103,54 +101,53 @@ public class DrivetrainSubsystem extends SubsystemBase {
   public DrivetrainSubsystem() {
     super();
     ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
+    positions = new SwerveModulePosition[]{m_frontLeftModule.getPosition(), m_frontRightModule.getPosition(), m_backLeftModule.getPosition(), m_backRightModule.getPosition()};
 
-    m_frontLeftModule = Mk4iSwerveModuleHelper.createFalcon500(
-            tab.getLayout("Front Left Module", BuiltInLayouts.kList)
-                    .withSize(2, 4)
-                    .withPosition(0, 0),
-            // This can either be STANDARD or FAST depending on your gear configuration
-            Mk4iSwerveModuleHelper.GearRatio.L2,
-            // This is the ID of the drive motor
-            FRONT_LEFT_MODULE_DRIVE_MOTOR,
-            // This is the ID of the steer motor
-            FRONT_LEFT_MODULE_STEER_MOTOR,
-            // This is the ID of the steer encoder
-            FRONT_LEFT_MODULE_STEER_ENCODER,
-            FRONT_LEFT_MODULE_STEER_OFFSET
-    );
+    m_frontLeftModule = new MkSwerveModuleBuilder()
+                .withLayout(tab.getLayout("Front Left Module", BuiltInLayouts.kList)
+                        .withSize(2, 4)
+                        .withPosition(0, 0))
+                .withGearRatio(SdsModuleConfigurations.MK4I_L2)
+                .withDriveMotor(MotorType.FALCON, Constants.FRONT_LEFT_MODULE_DRIVE_MOTOR)
+                .withSteerMotor(MotorType.FALCON, Constants.FRONT_LEFT_MODULE_STEER_MOTOR)
+                .withSteerEncoderPort(Constants.FRONT_LEFT_MODULE_STEER_ENCODER)
+                .withSteerOffset(Constants.FRONT_LEFT_MODULE_STEER_OFFSET)
+                .build();
   
-    m_frontRightModule = Mk4iSwerveModuleHelper.createFalcon500(
-            tab.getLayout("Front Right Module", BuiltInLayouts.kList)
-                    .withSize(2, 4)
-                    .withPosition(2, 0),
-            Mk4iSwerveModuleHelper.GearRatio.L2,
-            FRONT_RIGHT_MODULE_DRIVE_MOTOR,
-            FRONT_RIGHT_MODULE_STEER_MOTOR,
-            FRONT_RIGHT_MODULE_STEER_ENCODER,
-            FRONT_RIGHT_MODULE_STEER_OFFSET
-    );
+    m_frontRightModule = new MkSwerveModuleBuilder()
+                .withLayout(tab.getLayout("Front Right Module", BuiltInLayouts.kList)
+                        .withSize(2, 4)
+                        .withPosition(2, 0))
+                .withGearRatio(SdsModuleConfigurations.MK4I_L2)
+                .withDriveMotor(MotorType.FALCON, Constants.FRONT_RIGHT_MODULE_DRIVE_MOTOR)
+                .withSteerMotor(MotorType.FALCON, Constants.FRONT_RIGHT_MODULE_STEER_MOTOR)
+                .withSteerEncoderPort(Constants.FRONT_RIGHT_MODULE_STEER_ENCODER)
+                .withSteerOffset(Constants.FRONT_RIGHT_MODULE_STEER_OFFSET)
+                .build();
 
-    m_backLeftModule = Mk4iSwerveModuleHelper.createFalcon500(
-            tab.getLayout("Back Left Module", BuiltInLayouts.kList)
-                    .withSize(2, 4)
-                    .withPosition(4, 0),
-            Mk4iSwerveModuleHelper.GearRatio.L2,
-            BACK_LEFT_MODULE_DRIVE_MOTOR,
-            BACK_LEFT_MODULE_STEER_MOTOR,
-            BACK_LEFT_MODULE_STEER_ENCODER,
-            BACK_LEFT_MODULE_STEER_OFFSET
-    );
+    m_backLeftModule = new MkSwerveModuleBuilder()
+                .withLayout(tab.getLayout("Back Left Module", BuiltInLayouts.kList)
+                        .withSize(2, 4)
+                        .withPosition(4, 0))
+                .withGearRatio(SdsModuleConfigurations.MK4I_L2)
+                .withDriveMotor(MotorType.FALCON, Constants.BACK_LEFT_MODULE_DRIVE_MOTOR)
+                .withSteerMotor(MotorType.FALCON, Constants.BACK_LEFT_MODULE_STEER_MOTOR)
+                .withSteerEncoderPort(Constants.BACK_LEFT_MODULE_STEER_ENCODER)
+                .withSteerOffset(Constants.BACK_LEFT_MODULE_STEER_OFFSET)
+                .build();
+
         
-    m_backRightModule = Mk4iSwerveModuleHelper.createFalcon500(
-            tab.getLayout("Back Right Module", BuiltInLayouts.kList)
-                    .withSize(2, 4)
-                    .withPosition(6, 0),
-            Mk4iSwerveModuleHelper.GearRatio.L2,
-            BACK_RIGHT_MODULE_DRIVE_MOTOR,
-            BACK_RIGHT_MODULE_STEER_MOTOR,
-            BACK_RIGHT_MODULE_STEER_ENCODER,
-            BACK_RIGHT_MODULE_STEER_OFFSET
-    );
+    m_backRightModule = new MkSwerveModuleBuilder()
+                .withLayout(tab.getLayout("Back Right Module", BuiltInLayouts.kList)
+                        .withSize(2, 4)
+                        .withPosition(6, 0))
+                .withGearRatio(SdsModuleConfigurations.MK4I_L2)
+                .withDriveMotor(MotorType.FALCON, Constants.BACK_RIGHT_MODULE_DRIVE_MOTOR)
+                .withSteerMotor(MotorType.FALCON, Constants.BACK_RIGHT_MODULE_STEER_MOTOR)
+                .withSteerEncoderPort(Constants.BACK_RIGHT_MODULE_STEER_ENCODER)
+                .withSteerOffset(Constants.BACK_RIGHT_MODULE_STEER_OFFSET)
+                .build();
+        
   }
 
   /**
@@ -166,7 +163,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
 }
 
-  public Supplier<Pose2d> get_pose = () -> m_odometry.getEstimatedPosition(); // returns pose
+  public Supplier<Pose2d> get_pose = () -> m_odometry.getPoseMeters(); // returns pose
 
   public void drive(ChassisSpeeds chassisSpeeds) { // read name
     m_chassisSpeeds = chassisSpeeds;
@@ -176,8 +173,13 @@ public class DrivetrainSubsystem extends SubsystemBase {
         m_odometry.resetPosition(getGyroscopeRotation(), positions, pose);
   };
 
-  public void reset_poseMethod(Pose2d pose) { // read name
-        m_odometry.resetPosition(getGyroscopeRotation(), positions, pose);
+  public void resetPoseToPath(Pose2d pose) { // read name
+        m_odometry.resetPosition(
+                getGyroscopeRotation(),
+                new SwerveModulePosition[]{m_frontLeftModule.getPosition(), m_frontRightModule.getPosition(), m_backLeftModule.getPosition(), m_backRightModule.getPosition() },
+                pose
+        );
+;
   };
 
 
@@ -194,21 +196,16 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
-    second_timer++;
+    m_odometry.update(
+        getGyroscopeRotation(),
+        new SwerveModulePosition[]{ m_frontLeftModule.getPosition(), m_frontRightModule.getPosition(), m_backLeftModule.getPosition(), m_backRightModule.getPosition() }
+    );
+        
     SwerveModuleState[] states = m_kinematics.toSwerveModuleStates(m_chassisSpeeds);
 
-    SwerveDriveKinematics.desaturateWheelSpeeds(states, MAX_VELOCITY_METERS_PER_SECOND);
-
-
-    positions[0] = new SwerveModulePosition(m_frontLeftModule.getDriveVelocity() / 50 + positions[0].distanceMeters, new Rotation2d(m_frontLeftModule.getSteerAngle()));
-    positions[1] = new SwerveModulePosition(m_frontRightModule.getDriveVelocity() / 50 + positions[1].distanceMeters, new Rotation2d(m_frontRightModule.getSteerAngle()));
-    positions[2] = new SwerveModulePosition(m_backLeftModule.getDriveVelocity() / 50 + positions[2].distanceMeters, new Rotation2d(m_backLeftModule.getSteerAngle()));
-    positions[3] = new SwerveModulePosition(m_backRightModule.getDriveVelocity() / 50 + positions[3].distanceMeters, new Rotation2d(m_backRightModule.getSteerAngle()));
+    SwerveDriveKinematics.desaturateWheelSpeeds(states, MAX_VELOCITY_METERS_PER_SECOND);    
     
-    
-    m_pose = m_odometry.update(getGyroscopeRotation(), positions); // update odometry
     consume_states.accept(states);
-
   }
 
   public Command generatetrajectory(PathPlannerTrajectory traj, boolean isFirst){
@@ -216,7 +213,7 @@ public class DrivetrainSubsystem extends SubsystemBase {
                 new InstantCommand(() -> {
                   // Reset odometry for the first path you run during auto
                   if(isFirst){
-                      this.reset_poseMethod(traj.getInitialHolonomicPose());
+                      this.resetPoseToPath(traj.getInitialHolonomicPose());
                   }
                 }),
                 new PPSwerveControllerCommand(
